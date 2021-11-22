5aba2a8122f9a3fa8898e5d90461e930efb4d0c9
ibraaltabian17 <92589376+ibraaltabian17@users.noreply.github.com>
2021-11-22 17:36:45 +0700

    Re-start build

--- envsetup.sh
+++ envsetup.sh
@@ -1,11 +1,13 @@
 printf "\nMelakukan Envsetup\n"
+
 sudo apt update -y && sudo apt install git aria2 curl wget rsync zip unzip lzma cpio ccache clang -y
 sudo apt install python3 -y
 sudo ln -sf /usr/bin/python3 /usr/bin/python
 git config --global user.name "Ibratabian17"
 git config --global user.email "ibratabian@gmail.com"
+git config --global color.ui false
 git clone https://github.com/akhilnarang/scripts.git ~/scripts
 cd ~/scripts
 sudo bash setup/android_build_env.sh
 cd ~
-printf "\nBerhasil Melakukan envsetup\n"
+printf "================================================\nBerhasil Melakukan envsetup\n"
