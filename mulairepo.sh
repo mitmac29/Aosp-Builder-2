bff7ff4914b924b77955578aa15a5bf0233b05fb
ibraaltabian17 <92589376+ibraaltabian17@users.noreply.github.com>
2021-11-22 17:23:24 +0700

    Update mulairepo.sh

--- mulairepo.sh
+++ mulairepo.sh
@@ -1,5 +1,9 @@
 mkdir -p ~/work
 cd ~/work
 repo init -u git://github.com/ProjectHiper-ROM/android_manifest.git -b 11
+git clone https://github.com/ProjectHiper-ROM/android_device_xiaomi_beryllium device/xiaomi/beryllium
+git clone https://github.com/ProjectHiper-ROM/android_device_xiaomi_sdm845-common device/xiaomi/sdm845-common
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 kernel/xiaomi/sdm845
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 repo sync -j31 --force-sync --no-tags --no-clone-bundle
 echo done
